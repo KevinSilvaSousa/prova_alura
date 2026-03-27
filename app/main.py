0846a3e9be81a39f92d840acee6126f04f5d5723
Kevin Sousa <ksilvasousa2008@gmail.com>
2026-03-27 14:39:13 -0300

    commits

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title = "Catalago de Séries",
+    description = "Catalago",
+    version = "1.0.0"
+)
+
+@app.get ("/")
+def home():
+    return {"messagem": "Catalago de Séries em construção"}
+
